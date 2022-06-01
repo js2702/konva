f540e971b099468438a6bbd2567b9074fcb2b7be
Jorge Sardina <jorgesarpe@pm.me>
2022-06-01 11:47:13 +0200

    Upload build

--- konva.js
+++ konva.js
@@ -8,7 +8,7 @@
    * Konva JavaScript Framework v8.3.9
    * http://konvajs.org/
    * Licensed under the MIT
-   * Date: Mon May 30 2022
+   * Date: Wed Jun 01 2022
    *
    * Original work Copyright (C) 2011 - 2013 by Eric Rowell (KineticJS)
    * Modified work Copyright (C) 2014 - present by Anton Lavrenov (Konva)
